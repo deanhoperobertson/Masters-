aed57f933a17ad704ca935d980d4fd10702e328e
Dean Hope Robertson <deanhoperobertson@gmail.com>
2019-03-23 18:37:50 +0000

    Add files via upload

---
+++ Thesis/Code/prepro.py
@@ -0,0 +1,18 @@
+
+def readfile(filename):
+    f = open(filename)
+    sentences = []
+    sentence = []
+    for line in f:
+        if len(line) == 0 or line.startswith('-DOCSTART') or line[0] == "\n":
+            if len(sentence) > 0:
+                sentences.append(sentence)
+                sentence = []
+            continue
+        splits = line.split(' ')
+        sentence.append([splits[0], splits[-1].strip()])
+
+    if len(sentence) > 0:
+        sentences.append(sentence)
+        sentence = []
+    return sentences
