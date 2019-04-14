1a5c4e2138745c54e24be359aca526874bc719d5
Dean Hope Robertson <deanhoperobertson@gmail.com>
2019-04-14 17:12:38 +0100

    Add files via upload

--- Python Module/Assignment 2/analysis.py
+++ Python Module/Assignment 2/analysis.py
@@ -6,33 +6,68 @@ Titele: Analysis
 
 """
 #import packages
-import pandas as pd
+import csv
 
-
-#prompt user for file name
+#prompt use for file name
 filename=input('Enter the name of the CSV file:\n')
 
-#read csv file in
-data = pd.read_csv(filename)
+#read in the csv file
+file = open(filename,'r')
+data = csv.reader(file, delimiter=',')
+dictionary =  csv.DictReader(file, delimiter=',') #read in a dictionary to index from
 
-#rename columns
-data=data.rename(columns={"dam":"DAM NAME",
-                     " last_year": "LAST YEAR",
-                     " fsc":"FSC",
-                     " this_week":"THIS WEEK"})
+#get columns names in a list
+cols = dictionary.fieldnames
+cols = [x.strip() for x in cols]
 
-#subsect the data to only relevant columns 
-data=data[["THIS WEEK", "LAST YEAR", "FSC","DAM NAME"]]
+#retrieve indexes of the nessecary columns
+fsc_index = cols.index("fsc")
+this_week_index = cols.index("this_week")
+last_year_index = cols.index("last_year")
+names_index = cols.index("dam")
 
-#converting % to actual storage volumes
-data["THIS WEEK"] = round((data["THIS WEEK"]/100)*data["FSC"],1)
-data["LAST YEAR"] = round((data["LAST YEAR"]/100)*data["FSC"],1)
+#declare empty lists to be populated
+fsc = []
+this_week = []
+last_year = []
+names = []
 
-#summation of FSC figures
-total = round(data["FSC"].sum(),1)
-percentage = round((data["THIS WEEK"].sum()/total)*100,1) #convert to percentages
+#define function for list multuplication
+def multiply_list(a,b):
+    output=[]
+    for x,y in zip(a,b):
+        output.append(x*y)
+    return(output)
 
-## PRINT OUTPUT
-print(data)
-print("Total FSC is %s million cubic metres." %total)
+#run through data file extract the nessecary data points for specfic columns
+for file in data:
+    fsc.append(float(file[fsc_index]))
+    this_week.append(float(file[this_week_index]))
+    last_year.append(float(file[last_year_index]))
+    names.append(file[names_index])
+    
+#------------Calculations--------
+total = round(sum(fsc),1) #total FSC
+per = [x/100 for x in this_week] #this week as decimals 
+this_week = multiply_list(per,fsc) # list multiplication
+per = [x/100 for x in last_year]#last year as decimals
+last_year = multiply_list(per,fsc) # list multiplication
+percentage= round((sum(this_week)/total)*100,1)
+
+#create a dictionaries to print from
+dd = {}
+dd['THIS WEEK'] = [round(x,1) for x in this_week]
+dd['LAST YEAR'] = [round(x,1) for x in last_year]
+dd['FSC'] = fsc
+dd['DAM NAME'] = names
+cols = ['THIS WEEK','LAST YEAR','FSC','DAM NAME']
+
+##---------------OUTPUT----------
+#print columns
+print("{:>10}{:>10}{:>10}{:>9}".format(cols[0],cols[1], cols[2], cols[3]))
+
+#print data columns
+for i in range(len(dd['THIS WEEK'])):
+    print("{:>10}{:>10}{:>10}{:<10}".format(dd['THIS WEEK'][i],dd['LAST YEAR'][i],dd['FSC'][i]," "+dd['DAM NAME'][i]))
+print("\nTotal FSC is %s million cubic metres." %total)
 print("Currently at {}%.".format(percentage))
--- Python Module/Assignment 2/query.py
+++ Python Module/Assignment 2/query.py
@@ -1,166 +1,115 @@
-
-"""
-#AUTHOR = Dean Hope Robertson
-Std Number: RBRDEA003
-Titele: Analysis
-
-"""
-
 #packages
-import pandas as pd
 from datetime import time
 
-#------------ DEFINE FUCNTIONS---------
+#functions----------
 def print_list():
-    #prints out the list of commands if needed 
     print("Available commands:\nload <route number>\nnext <embark_point> <from_time>\narrival <embark_point> <embark_time> <destination>\ndepartures <embark_point> <destination> <earliest_arrival> <latest_arrival>")
-    
-def departure_exist(place,file_input):
-    places =[]
-    for line in file_input:
-        places.append(line.split(" %")[0])
-    if place in places:
-        return(True)
-    else:
-        return(False)
-
 
 def load_data(txt_file):
-    #fucntion reads in the file(csv format) and create a dataframe
-    #The function then reformats the dataframe into a usable format.
-    data = pd.read_csv(txt_file, header=None)
-    new = data[0].str.split(" %", expand=True)
-    data.drop(columns =[0], inplace = True) 
-    data[0] = new[1]
-    data["Place"] = new[0]
-    data=data[["Place",0,1,2,3,4,5]]
-    data = data.set_index("Place")
-    return(data)
+    rw_file = open(txt_file,'r')
+    lines = rw_file.readlines()
+    rw_file.close()
+    places = []
+    data = []
+    for line in lines:
+        places.append(line.lstrip().split()[0])
+        data.append(line.lstrip().split("%")[1].rstrip().lstrip())
+    return(places, data)
 
 def convert_time(t1):
-    #converts a string and into object of datetime.time
-    hour = int(t1.split(":")[0]) #extract hours
-    mins = int(t1.split(":")[1]) #extract mins
+    hour = int(t1.split(":")[0])
+    mins = int(t1.split(":")[1])
     t1=time(hour,mins)
     return(t1)
 
 def next_time(command):
-    # The function is called when the user executes the "Next" command
-    # extracts place and time from string inout by user
-
     place = command.split()[1]
     t1 = convert_time(command.split()[2])
-    #get the times for the requested embark station
-    times = list(data.loc[place]) 
-    converted_times = [convert_time(time.strip()) for time in times] #convert times
-    #conditional list comprehension to remove the times that have already past 
-    converted_times = [x for x in converted_times if x >= t1] 
+    index = places.index(place)
+    times = data[index].split(",")
+    converted_times = [convert_time(time.strip()) for time in times]
+    converted_times = [x for x in converted_times if x >= t1]
+
     if len(converted_times) ==0:
-        #if no route available, then print:
         print("Sorry, no departures found")
     else: 
-        #return the next time available
         print("The next bus departing from %s is at %s" %(place,converted_times[0].strftime('%H:%M')))
-        
-def arrival(command): 
-    # The function is called when the user executes the "Arrival" command
-    # extracts both destination,embark station, and preferred time to leave 
 
+def arrival(command): 
     place1 = command.split()[1]
     place2 = command.split()[3]
     t1 = convert_time(command.split()[2])
-    #get the times for the requested embark station
-    times1 = list(data.loc[place1])
-    converted_times1 = [convert_time(time.strip()) for time in times1]
-    #conditional list comprehension to remove the times that have already past 
-    converted_times1 = [x for x in converted_times1 if x >= t1]
-    #get the times for the requested arrival station
-    times2 = list(data.loc[place2])
+    index = places.index(place1)
+    times1 = data[index].split(",")
+    converted_times = [convert_time(time.strip()) for time in times1]
+    converted_times1 = [x for x in converted_times if x >= t1]
+    index = places.index(place2)
+    times2 = data[index].split(",")
     converted_times2 = [convert_time(time.strip()) for time in times2]
-    #extract the arival time of the bus that arrives at the destination
     arrival_time=converted_times2[-len(converted_times1):][0]
 
     if len(converted_times1) ==0:
-        #if no route available, then print:
-        print("Sorry, no departures found")
+            print("Sorry, no departures found")
     else: 
-        #return the next the ariival time of the next bus leaving embrak station to destination
         print("The next bus departing from %s arrives at %s at %s" %(place1,place2,arrival_time.strftime('%H:%M')))
-    
+        
 def convert_time_list(t2):
-    #convert times to new %H:%M format for printing
     return(t2.strftime('%H:%M'))
 
 def departures(command):
-    # extracts both destination,embark station, earliest arrival and latest arrival times
     place1 = command.split()[1]
     place2 = command.split()[2]
     t1 = convert_time(command.split()[3])
     t2 = convert_time(command.split()[4])
-    #get the times for the requested destination station
-    times2 = list(data.loc[place2])
+    index = places.index(place2)
+    times2 = data[index].split(",")
     converted_times= [convert_time(time.strip()) for time in times2]
-    #conditional list comprehension to find the times between early-late arrival
     converted_times2 = [x for x in converted_times if x >= t1 and x <=t2]
+    try:
+        loc1 = converted_times.index(converted_times2[0])
+        loc2 = converted_times.index(converted_times2[-1])
+        #departures
+        index = places.index(place1)
+        times1 = data[index].split(",")
+        converted_times1 = [convert_time(time.strip()) for time in times1]
 
-    loc1 = converted_times.index(converted_times2[0])
-    loc2 = converted_times.index(converted_times2[-1])
+        ouput_times = converted_times1[loc1:loc2+1]
+        ouput_times = [convert_time_list(x) for x in ouput_times]
 
-    #departures
-    times1 = list(data.loc[place1])
-    converted_times1 = [convert_time(time.strip()) for time in times1]
-    ouput_times = converted_times1[loc1:loc2+1]
-    #cconvert times to new format
-    ouput_times = [convert_time_list(x) for x in ouput_times]
-
-    if len(ouput_times) ==0:
+        if len(ouput_times) ==0:
             print("Sorry, no departures found")
-    else: 
-        #return the next the bus time of the bus from embrak station to destination in that period
-        print("Buses arriving at %s in that period depart from %s at"%(place2,place1))
-        print(*ouput_times,sep = ", ")
-
+        else: 
+            print("Buses arriving at %s in that period depart from %s at"%(place2,place1),', '.join(map(str, ouput_times)))
+    except (IndexError):
+        print("Sorry, no departures found")
 
 status = "running"
 
-#prompt user for command
-print("please enter command:\n")
-
-#initialize while loop
 while status is "running":
     command = input(">")
-
-    #if "quit" command is executed
     if command =="quit":
-        status="quit" #change status
-
-    #if "help" command is executed
+        status="quit"
     elif command == "help":
         print_list()
-    
-    #if "load" command is executed
+        #break
     elif command.split()[0] == "load":
-        data = load_data(command.split()[1]+".txt")
+        places,data = load_data(command.split()[1]+".txt")
         print("Done")
     
-    #if "next" command is executed
     elif command.split()[0] == "next":
         try:
             next_time(command)
         except NameError:
             print("You must load a route first.")
-    
-    #if "arrival" command is executed
+
     elif command.split()[0] == "arrival":
         try:
             arrival(command)
         except NameError:
             print("You must load a route first.")
     
-    #if "departures" command is executed
     elif command.split()[0] == "departures":
         try:
             departures(command)
         except NameError:
             print("You must load a route first.")
-
